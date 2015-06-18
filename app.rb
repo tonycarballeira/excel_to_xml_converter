b86f76ecd720466c5943694e7a61e2925c594db9
tonycarballeira <tonycarballeira@gmail.com>
2015-06-17 22:57:43 -0400

    made helper method for xml conversion

--- app.rb
+++ app.rb
@@ -4,14 +4,12 @@ require "spreadsheet"
 require "nokogiri"
 require "sinatra/cookies"	
 
-enable :sessions
+require_relative "helpers"
+helpers Helpers
 
 Spreadsheet.client_encoding = 'UTF-8'
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
-before do
-	@file_names = []
-end
 
 get "/"  do
 	erb :index
@@ -28,33 +26,7 @@ post "/" do
 end
 
 get "/xml" do
-	@file_name = cookies[:filename] 
-  @book = Spreadsheet.open('uploads/'+@file_name)
-  @sheets = @book.worksheets
-  @rows = []
-	  
-	  @sheets.each do |sheet| 
-		sheet.each do |row| 
-			 @rows << row 
-			end
-		end
-
-
-	@builder = Nokogiri::XML::Builder.new do |xml|
-	  xml.root {
-	    xml.records {
-		      @rows.each do |o|
-	        xml.record {
-	          xml.name_   o[0]
-	          xml.month_  o[1]
-	          xml.day_    o[2]
-	          xml.year_   o[3]
-	        }
-	      end
-	    }
-	  }
-	end
-
+	@file_name = cookies[:filename]
+	convert_excel(@file_name)
   erb :show_image
-
 end
